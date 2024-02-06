1738feb1df1c32f19ef61e0dc8e10ec01cea24c7
jailan-sh <jilan.elsherbiny@gmail.com>
2024-02-06 03:18:09 +0200

    10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-	for (var idx in array) {
-	  var value = array[idx];
-	  array[idx] = appendString + value;
-	}
-  
-	return array;
+  for (const [i, value] of array.entries()) {
+    array[i] = appendString + value;
   }
+  return array;
+}
