366624375a4dd05e4a7956b8191b182eca4f2d24
jailan-sh <jilan.elsherbiny@gmail.com>
2024-03-09 14:14:41 +0200

    2,4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') this._amount = amount;
+    else throw TypeError('Amount must be a number');
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) this._currency = currency;
+    else throw TypeError('currency must be an instanceof Currency');
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
