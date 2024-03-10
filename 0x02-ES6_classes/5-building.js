4beb2ba1a115b60c9ad26ece25bd25778c5d9b1c
jailan-sh <jilan.elsherbiny@gmail.com>
2024-03-10 02:07:15 +0200

    abstract class, method

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
+    if (
+      this.constructor !== Building &&
+      typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft === 'number') this._sqft = sqft;
-    else throw TypeError('sqft must be a number');
-  }
-
-  evacuationWarningMessage() {
-    if (this.constructor === Building) {
-      throw Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
-    }
   }
 }
