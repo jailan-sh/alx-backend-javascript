51e9737a2370c1e9847acb5682c48dd60e6ee5e1
jailan-sh <jilan.elsherbiny@gmail.com>
2024-03-14 02:49:57 +0200

    _

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -2,7 +2,7 @@ export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
-    this.color = color;
+    this._color = color;
   }
 
   static get [Symbol.species]() {
