6225d3ebd3dd2504a296a05fe7564c182a089a96
jailan-sh <jilan.elsherbiny@gmail.com>
2024-03-29 21:03:46 +0200

    6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,4 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstNmae, lastName, fileNmae) {}
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const data = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+  console.log(data);
+}
